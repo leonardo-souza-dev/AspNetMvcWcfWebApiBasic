5a08deed459eacb88c267562718feb7dd817c933
leona <leonardotreze@gmail.com>
2017-03-20 17:49:57 -0300

    angular

---
+++ TesteLeonardo.UI/Scripts/app/testeLeonardoApp.js
@@ -0,0 +1,9 @@
+﻿
+(function () {
+    
+    var app = angular.module('testeLeonardoApp', ['ngRoute']);
+
+    app.controller('HomeController', function ($scope) {
+        $scope.Mensagem = "Foo";
+    });
+})();

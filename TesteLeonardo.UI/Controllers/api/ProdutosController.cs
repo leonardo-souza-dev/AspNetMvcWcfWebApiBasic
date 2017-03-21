9652cbd1003113b7c34e7842e945686a7d650957
leona <leonardotreze@gmail.com>
2017-03-20 23:41:26 -0300

    ajustes

---
+++ TesteLeonardo.UI/Controllers/api/ProdutosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TesteLeonardo.Dominio;
+using TesteLeonardo.Interface;
+
+namespace TesteLeonardo.UI.Api.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        private readonly IProdutoRepositorio ProdutoRepositorio;
+
+        public ProdutosController(IProdutoRepositorio produtoRepositorio)
+        {
+            this.ProdutoRepositorio = produtoRepositorio;
+        }
+
+        public IEnumerable<Produto> Get()
+        {
+            var produtos = ProdutoRepositorio.ObterProdutos();
+
+            return produtos;
+        }
+    }
+}
